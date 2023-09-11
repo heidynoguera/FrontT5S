2c3e07f1d4222f8ed9944cc5ec439a5e7eeda538
DfGarcia21 <dfgarciab@itc.edu.co>
2023-09-10 21:21:22 -0500

    Elimine valoracion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ValoracionComponent } from './components/valoracion/valoracion.component';
 import { FormaPagoComponent } from './components/forma-pago/forma-pago.component';
 import { CalendarioComponent } from './components/calendario/calendario.component';
 
 const routes: Routes = [
-  {path: "valoracion", component: ValoracionComponent},
   {path: "formapago", component: FormaPagoComponent},
   {path: "calendario", component: CalendarioComponent}
 ];
