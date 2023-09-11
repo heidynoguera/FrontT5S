f06db34412fe56ca10a5268264e6d7bdfc23a748
DfGarcia21 <dfgarciab@itc.edu.co>
2023-09-10 19:40:21 -0500

    Se agrego routing de: calendario, valoracion y forma de pago

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValoracionComponent } from './components/valoracion/valoracion.component';
+import { FormaPagoComponent } from './components/forma-pago/forma-pago.component';
+import { CalendarioComponent } from './components/calendario/calendario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "valoracion", component: ValoracionComponent},
+  {path: "formapago", component: FormaPagoComponent},
+  {path: "calendario", component: CalendarioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
-
-  constructor() { }
+  //inyecccion de dependencias
+  URL = "https://localhost:7149/api/"
+  constructor(public api: HttpClient) { }
+  public async Get(controller: string) {
+    await this.api.get(this.URL + controller).toPromise().then((res) => {
+      console.log(res);
+    });
+  }
 }
