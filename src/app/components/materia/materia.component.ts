5829e04c771abd3cec15acb8023a88012e7f171e
Andrmel1 <141949817+Andrmel1@users.noreply.github.com>
2023-09-10 20:48:16 -0500

    Se realiza creación de Onit de Tutor, Materia y Tutor Materia

--- src/app/components/materia/materia.component.ts
+++ src/app/components/materia/materia.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
+
 
 @Component({
   selector: 'app-materia',
   templateUrl: './materia.component.html',
   styleUrls: ['./materia.component.css']
 })
-export class MateriaComponent {
+export class MateriaComponent implements OnInit {
+  
+  constructor(public api: RestService){
 
+  }
+  ngOnInit(): void{
+    this.get();
+  }
+
+  public get (){
+    this.api.Get("Materia");
+  }
 }
--- src/app/components/tutor/tutor.component.ts
+++ src/app/components/tutor/tutor.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-tutor',
   templateUrl: './tutor.component.html',
   styleUrls: ['./tutor.component.css']
 })
-export class TutorComponent {
+export class TutorComponent implements OnInit{
 
+    constructor(public api: RestService){
+  
+    }
+    ngOnInit(): void{
+      this.get();
+    }
+  
+    public get (){
+      this.api.Get("Tutor");
+    }
 }
