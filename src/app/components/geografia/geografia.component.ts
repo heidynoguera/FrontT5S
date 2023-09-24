d31bd2dbbf375407347dbded038812b5592c98be
heidynoguera <heidynoguera37@gmail.com>
2023-09-24 17:51:17 -0500

    generacion de tablas,filtro y paginacion controladores geografia,reservatutoria

--- src/app/components/geografia/geografia.component.ts
+++ src/app/components/geografia/geografia.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { RestService } from 'src/app/services/rest.service';
 
 @Component({
@@ -6,15 +9,64 @@ import { RestService } from 'src/app/services/rest.service';
   templateUrl: './geografia.component.html',
   styleUrls: ['./geografia.component.css']
 })
-export class GeografiaComponent implements OnInit{
-  constructor(public api: RestService){
-  }
-  ngOnInit(): void{
-    this.get();
+export class GeografiaComponent implements OnInit ,AfterViewInit{
+  displayedColumns: string[] = [];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(public api: RestService) {
+    this.dataSource = new MatTableDataSource();
   }
 
-  public get (){
+  ngOnInit(): void {
+    this.api.Get("Geografiums").then((res) => {
+
+      for (let index = 0; index < res.length; index++) {
+        this.loadTable([res[index]])
+      }
+      this.dataSource.data = res;
+    })
+  }
+
+  ngAfterViewInit():void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  loadTable(data: any[]) {
+    this.displayedColumns = [];
+    for (let column in data[0]) {
+      this.displayedColumns.push(column)
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  public get() {
     this.api.Get("Geografiums");
   }
 
+//actualizar datos
+  public putGeografia(idGeografia: number) { 
+    const newData = { /* tus datos a actualizar */ };
+    this.api.Put("Geografiums", idGeografia, newData);
+  }
+  //crear datos
+  public postGeografia(newGeografia : any) {
+    this.api.Post("Geografiums", newGeografia);
+  }
+  //borrar datos
+  public async deleteGeografia(IdGeografia: string) {
+    const response = await this.api.Delete("Geografiums", IdGeografia);
+}
 }
--- src/app/components/reservar-tutoria/reservar-tutoria.component.ts
+++ src/app/components/reservar-tutoria/reservar-tutoria.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { RestService } from 'src/app/services/rest.service';
 
 @Component({
@@ -6,14 +9,63 @@ import { RestService } from 'src/app/services/rest.service';
   templateUrl: './reservar-tutoria.component.html',
   styleUrls: ['./reservar-tutoria.component.css']
 })
-export class ReservarTutoriaComponent implements OnInit{
+export class ReservarTutoriaComponent implements OnInit, AfterViewInit{
+  displayedColumns: string[] = [];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(public api: RestService){
+    this.dataSource = new MatTableDataSource();
   }
+
   ngOnInit(): void{
-    this.get();
+   this.api.Get("ResevarTutoriums").then((res) => {
+
+    for (let index = 0; index < res.length; index++) {
+      this.loadTable([res[index]])
+    }
+    this.dataSource.data = res;
+  })
+  }
+
+  ngAfterViewInit():void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  loadTable(data: any[]) {
+    this.displayedColumns = [];
+    for (let column in data[0]) {
+      this.displayedColumns.push(column)
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   public get (){
-    this.api.Get("reservartutotiums");
+    this.api.Get("ResevarTutoriums");
   }
+  //actualizar datos
+  public putReservTuto(idReserva: number) { 
+    const newData = { /* tus datos a actualizar */ };
+    this.api.Put("ResevarTutoriums", idReserva, newData);
+  }
+  //crear datos
+  public postReservTuto(newReserva : any) {
+    this.api.Post("ResevarTutoriums", newReserva);
+  }
+  //borrar datos
+  public async deleteReservTuto(idReserva: string) {
+    const response = await this.api.Delete("ResevarTutoriums", idReserva);
+}
 }
