b9f4145a9d12cdddf9bb49e954fcea5df9346634
AndreaTibamosca <adgonzalezt@itc.edu.co>
2023-09-25 20:15:06 -0500

    actualizacion tablas estudiantes y login

--- src/app/components/estudiante/estudiante.component.ts
+++ src/app/components/estudiante/estudiante.component.ts
@@ -1,5 +1,8 @@
 
-import { Component, OnInit } from '@angular/core'; 
+import { AfterViewInit, Component, OnInit,ViewChild  } from '@angular/core'; 
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { RestService } from 'src/app/services/rest.service';
 
 
@@ -8,14 +11,32 @@ import { RestService } from 'src/app/services/rest.service';
   templateUrl: './estudiante.component.html',
   styleUrls: ['./estudiante.component.css']
 })
-export class EstudianteComponent implements OnInit{
+export class EstudianteComponent implements OnInit, AfterViewInit{
+
+  displayedColumns: string[] = [];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(public api: RestService){
+        this.dataSource = new MatTableDataSource();
+
 
   }
 
   ngOnInit(): void{
-    this.get();
+    //this.get();
+
+    this.api.Get("estudiantes").then((res)=>{
+
+      for(let index = 0; index < res.length; index++){
+        this.loadTable([res[index]])
+      }
+
+      this.dataSource.data= res;
+
+    })
 
     // const nuevoEstudiante = {
     //   apellidoEst:"GONZALEZ",
@@ -35,6 +56,24 @@ export class EstudianteComponent implements OnInit{
     // };
 
     // this.crearEstudiante(nuevoEstudiante);
+  }
+
+  loadTable(data:any[]){
+    this.displayedColumns=[];
+    for(let column in data[0]){
+      this.displayedColumns.push(column)
+    }
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   public crearEstudiante(nuevoEstudiante : any) {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { RestService } from 'src/app/services/rest.service';
 
 @Component({
@@ -6,16 +9,55 @@ import { RestService } from 'src/app/services/rest.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit, AfterViewInit{
+
+  displayedColumns: string[] = [];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(public api: RestService){
 
+    this.dataSource = new MatTableDataSource();
+
   }
   ngOnInit(): void{
-    this.get();
+    //this.get();
+    this.api.Get("logins").then((res)=>{
+
+      for(let index = 0; index < res.length; index++){
+        this.loadTable([res[index]])
+      }
+
+      this.dataSource.data= res;
+
+    })
   }
 
   public get (){
     this.api.Get("logins");
   }
+  loadTable(data:any[]){
+    this.displayedColumns=[];
+    for(let column in data[0]){
+      this.displayedColumns.push(column)
+    }
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  
+ 
+  
 }
+
