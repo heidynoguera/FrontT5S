a2bca2bfe524fa0e57ff278d504eb8d0b3f93e6b
heidynoguera <heidynoguera37@gmail.com>
2023-10-25 13:13:08 -0500

    actualizacion css ,alerts y agregacion de forms geografia,repositorio,reservatutoria

--- src/app/components/geografia/geografia.component.ts
+++ src/app/components/geografia/geografia.component.ts
@@ -3,7 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { RestService } from 'src/app/services/rest.service';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { FormGeografiaComponent } from 'src/app/Form/form-geografia/form-geografia.component';
 import Swal from 'sweetalert2';
 
@@ -12,7 +12,7 @@ import Swal from 'sweetalert2';
   templateUrl: './geografia.component.html',
   styleUrls: ['./geografia.component.css']
 })
-export class GeografiaComponent implements OnInit ,AfterViewInit{
+export class GeografiaComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [];
   dataSource: MatTableDataSource<any>;
 
@@ -20,7 +20,7 @@ export class GeografiaComponent implements OnInit ,AfterViewInit{
   paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(public api: RestService,public dialog: MatDialog) {
+  constructor(public api: RestService, public dialog: MatDialog) {
     this.dataSource = new MatTableDataSource();
   }
 
@@ -34,9 +34,26 @@ export class GeografiaComponent implements OnInit ,AfterViewInit{
     })
   }
 
-  ngAfterViewInit():void {
+  ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.dataSource.paginator = this.paginator;
+
+    // Cambiar el formato de la etiqueta de rango directamente
+
+    this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+
+      if (length === 0 || pageSize === 0) {
+        return `0 de ${length}`;
+
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+      return `${startIndex + 1} - ${endIndex} de ${length}`;
+
+    };
   }
 
   loadTable(data: any[]) {
@@ -44,7 +61,7 @@ export class GeografiaComponent implements OnInit ,AfterViewInit{
     for (let column in data[0]) {
       this.displayedColumns.push(column)
     }
-    this.displayedColumns.push("Editar", "Delete") 
+    this.displayedColumns.push("Editar", "Delete")
   }
 
   applyFilter(event: Event) {
@@ -61,61 +78,61 @@ export class GeografiaComponent implements OnInit ,AfterViewInit{
   }
   openDialog() {
     const dialogRef = this.dialog.open(FormGeografiaComponent);
-  
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
   }
-  
-//actualizar datos
-  public putGeografia(idGeografia: number) { 
+
+  //actualizar datos
+  public putGeografia(idGeografia: number) {
     const newData = { /* tus datos a actualizar */ };
     this.api.Put("Geografiums", idGeografia, newData);
   }
   //crear datos
-  public postGeografia(newGeografia : any) {
+  public postGeografia(newGeografia: any) {
     this.api.Post("Geografiums", newGeografia);
   }
   //borrar datos
   //public async deleteGeografia(IdGeografia: string) {
- //   const response = await this.api.Delete("Geografiums", IdGeografia);
-//}
-mostrarNotificacionDelete() {
-  // Verificar si el navegador soporta las notificaciones
-  Swal.fire({
-    title: '¿Esta seguro?',
-    text: "No podrás revertir esto",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Si, Bórralo!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Borrado!',
-        'El elemento ha sido borrado.',
-        'success'
-      )
-    }
-  })
-}
+  //   const response = await this.api.Delete("Geografiums", IdGeografia);
+  //}
+  mostrarNotificacionDelete() {
+    // Verificar si el navegador soporta las notificaciones
+    Swal.fire({
+      title: '¿Esta seguro?',
+      text: "No podrás revertir esto",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Bórralo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Borrado!',
+          'El elemento ha sido borrado.',
+          'success'
+        )
+      }
+    })
+  }
 
-mostrarNotificacionEdit() {
-  // Verificar si el navegador soporta las notificaciones
-  Swal.fire({
-    title: '¿Deseas guardar los cambios?',
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: 'Guardar',
-    denyButtonText: `No Guardar`,
-  }).then((result) => {
-    /* Read more about isConfirmed, isDenied below */
-    if (result.isConfirmed) {
-      Swal.fire('Guardado!', '', 'success')
-    } else if (result.isDenied) {
-      Swal.fire('Los cambios no se han guardado', '', 'info')
-    }
-  })
-}
+  mostrarNotificacionEdit() {
+    // Verificar si el navegador soporta las notificaciones
+    Swal.fire({
+      title: '¿Deseas guardar los cambios?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Guardar',
+      denyButtonText: `No Guardar`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Guardado!', '', 'success')
+      } else if (result.isDenied) {
+        Swal.fire('Los cambios no se han guardado', '', 'info')
+      }
+    })
+  }
 }
--- src/app/components/reservar-tutoria/reservar-tutoria.component.ts
+++ src/app/components/reservar-tutoria/reservar-tutoria.component.ts
@@ -32,9 +32,26 @@ export class ReservarTutoriaComponent implements OnInit, AfterViewInit{
   })
   }
 
-  ngAfterViewInit():void {
+  ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.dataSource.paginator = this.paginator;
+
+    // Cambiar el formato de la etiqueta de rango directamente
+
+    this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+
+      if (length === 0 || pageSize === 0) {
+        return `0 de ${length}`;
+
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+      return `${startIndex + 1} - ${endIndex} de ${length}`;
+
+    };
   }
 
   loadTable(data: any[]) {
